3ebc184334b4f4d4fa717faad895060e29e8b674
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-17 13:29:14 -0300

    feat: add tsup and new components

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,1 +1,8 @@
 export * from './colors';
+export * from './space'
+export * from './radii'
+
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
