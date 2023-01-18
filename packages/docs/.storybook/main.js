532af11bdc46f2de4938b41a6734b8379027a0fb
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-18 15:32:54 -0300

    fix: deploy config

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -13,6 +13,9 @@ module.exports = {
   "core": {
     "builder": "@storybook/builder-vite"
   },
+  "features": {
+    "storyStoreV7": true
+  },
   viteFinal: (config, { configType }) => {
     if (configType === 'PRODUCTION') {
       config.base = '/05-design-system/'
