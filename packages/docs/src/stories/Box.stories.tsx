ca633b709133dad20493c57c30e0e27339370521
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-18 12:44:41 -0300

    feat: Heading and avatar component

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -2,11 +2,11 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Box, BoxProps, Text } from '@srsouzaj/react'
 
 export default {
-    title: 'Surfaces/Box',
-    component: Box,
-    args: {
-        children: <Text>Testando o elemento Box</Text>,
-    },
+  title: 'Surfaces/Box',
+  component: Box,
+  args: {
+    children: <Text>Testando o elemento Box</Text>,
+  },
 } as Meta<BoxProps>
 
 export const Primary: StoryObj<BoxProps> = {}
