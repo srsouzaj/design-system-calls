a0b67db7afe9e79b0fa0990780ecdb1f2b5d9874
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-18 13:47:41 -0300

    feat: add textInput

--- packages/react/src/components/TextInput/index.tsx
+++ packages/react/src/components/TextInput/index.tsx
@@ -2,14 +2,14 @@ import { ComponentProps } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
-    prefix?: string
+  prefix?: string
 }
 
 export function TextInput({ prefix, ...props }: TextInputProps) {
-    return (
-        <TextInputContainer>
-            {!!prefix && <Prefix>{prefix}</Prefix>}
-            <Input {...props} />
-        </TextInputContainer>
-    )
+  return (
+    <TextInputContainer>
+      {!!prefix && <Prefix>{prefix}</Prefix>}
+      <Input {...props} />
+    </TextInputContainer>
+  )
 }
