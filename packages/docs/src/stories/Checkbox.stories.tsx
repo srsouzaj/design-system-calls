bde79702dbb081b434de0583b16290d789958f96
Jorge de Souza <Jorgejrdj1@gmail.com>
2023-01-27 17:44:57 -0300

    fix: deploy

--- packages/docs/src/stories/Checkbox.stories.tsx
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -2,22 +2,22 @@ import type { Meta, StoryObj } from '@storybook/react'
 import { Box, Text, Checkbox, CheckboxProps } from '@srsouza/react'
 
 export default {
-    title: 'Form/Checkbox',
-    component: Checkbox,
-    args: {},
-    decorators: [
-        (Story) => {
-            return (
-                <Box
-                    as="label"
-                    css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
-                >
-                    {Story()}
-                    <Text size="sm">Accept terms of use?</Text>
-                </Box>
-            )
-        },
-    ],
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => {
+      return (
+        <Box
+          as="label"
+          css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+        >
+          {Story()}
+          <Text size="sm">Accept terms of use?</Text>
+        </Box>
+      )
+    },
+  ],
 } as Meta<CheckboxProps>
 
 export const Primary: StoryObj<CheckboxProps> = {}
